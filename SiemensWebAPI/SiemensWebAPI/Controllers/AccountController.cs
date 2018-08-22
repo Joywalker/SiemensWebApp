af3e2a2824eff0b911a22a29f1a0c3a5dd59ea48
CTBAV\alexandru.razvant <alexandru.razvant@ctbav.ro>
2018-08-22 16:58:08 +0300

    Added basic login functionality. Added user-view-model class.

---
+++ SiemensWebAPI/SiemensWebAPI/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Cors;
+using System.Web.Http;
+using SiemensWebAPI.Models;
+
+namespace SiemensWebAPI.Controllers
+{
+    public class AccountController : BaseController
+    {
+        [Route("api/User/Login")]
+        [HttpPost]
+        public IHttpActionResult Login(UserViewModel usr)
+        {
+            try
+            {
+                using (DatabaseContext dbctx = new DatabaseContext())
+                {
+                    String userRole = dbctx.UserAccounts.Where(usn => usn.Username.Equals(usr.Username))
+                                                        .Where(usp => usp.Password.Equals(usr.Password))
+                                                        .Select(column => column.UserRole).First();
+                    if (userRole == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        return Ok(userRole);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // treat ex
+            }
+            return Ok();
+        }
+    }
+}
