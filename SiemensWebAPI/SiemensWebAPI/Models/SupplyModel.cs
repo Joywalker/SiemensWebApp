4f6cb17aff03ededbf34c4efcbe8894d7243bdb9
CTBAV\alexandru.razvant <alexandru.razvant@ctbav.ro>
2018-08-22 16:53:21 +0300

    First commit. Added database models and generated models.

---
+++ SiemensWebAPI/SiemensWebAPI/Models/SupplyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SiemensWebAPI.Models
+{
+    public class SupplyModel
+    {
+        public int IDRawMaterial { get; set; }
+        public DateTime DateOfResupply { get; set; }
+        public int Quantity { get; set;}
+        
+    }
+}
---
+++ SiemensWebAPI/SiemensWebAPI/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiemensWebAPI.Models
+{
+    public class UserModel
+    {
+        public String ID { get; set; }
+        public String Username { get
+            {
+                return Username;
+            } }
+        public String Type {
+            get
+            {
+                return Type;
+            }}
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String CNP { get; set; }
+        public String Password {
+            get
+            {
+                return Password;
+            }
+        }
+    }
+}
